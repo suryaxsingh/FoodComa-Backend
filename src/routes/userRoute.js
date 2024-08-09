6ae756485accac765dfb598d88ea345f3093af15
suryaxsingh <suryasingh7022@gmail.com>
2024-08-09 08:27:52 +0530

    Added create user api

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const { createUser } = require('../controllers/userController');
 
-
-
 const userRouter = express.Router();
 
 userRouter.post('/', createUser);
